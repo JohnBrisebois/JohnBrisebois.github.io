398e5896d8515176499ff6cee453fce5c58e201d
JohnBrisebois <blenderjohn15@gmail.com>
2024-04-02 23:32:46 -0700

    swipe

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var pageArr = document.getElementsByClassName("pageitem");
 var pageIndex = 0;
 pageArr[pageIndex].style.display = "inline";
 page.addEventListener("wheel",scroll);
+page.addEventListener("swipe",scroll);
 down.addEventListener("click", clickDown);
 up.addEventListener("click", clickUp);
 var pageTop = 0;
@@ -25,6 +26,7 @@ function scrollVis(curr) {
         page.addEventListener("wheel", scroll);
         down.addEventListener("click", clickDown);
         up.addEventListener("click", clickUp);
+        page.addEventListener("swipe",scroll);
     }, 300);
 }
 
@@ -43,6 +45,7 @@ function scroll(event) {
         page.removeEventListener("wheel", scroll)
         down.removeEventListener("click", clickDown);
         up.removeEventListener("click", clickUp);
+        page.removeEventListener("swipe",scroll);
         let curr = pageIndex;
         anim = setInterval(function() {scrollAnim(curr, 1)}, 25);
         scrollVis(pageArr[curr])
@@ -53,6 +56,7 @@ function scroll(event) {
         page.removeEventListener("wheel", scroll);
         down.removeEventListener("click", clickDown);
         up.removeEventListener("click", clickUp);
+        page.removeEventListener("swipe",scroll);
         let curr = pageIndex;
         let currTop = pageArr[curr].offsetTop;
         anim = setInterval(function() {scrollAnim(curr, -1)}, 25);
